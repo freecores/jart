f1587add3fa4e8a494f1176f8a958bdc9a24dc37
jguarin2002 <jguarin2002@1cf26571-75e7-40ab-8c61-d213c3aa32ad>
2009-10-04 14:51:46 +0000

    Minor width parameters name change.
    Scan Out support.
    
    Register constant value parameter at cell exit. If contant value input to the cell doesn't go out, this is useful for the last grid in the array grid and saves some registers.
    
    K now flows upwards to: a K pipe flowing to the distance comparison row was added.

--- BLRT/kComparisonCell.vhd
+++ BLRT/kComparisonCell.vhd
@@ -29,74 +29,97 @@ use work.powerGrid.all;
 
 
 entity kComparisonCell is
-	generic (	W 		: integer := 32	);
+	generic (	
+			RK	: string	:= "yes";
+			W1	: integer	:= 32	
+	);
 	port (		
-				clk,rst		: in std_logic;
-	
-				nxtSphere	: in std_logic; -- Controls when the sphere goes to the next Row. 
-				pipeOn		: in std_logic; -- Enables / Disable the upwarding flow.
-				kinput		: in std_logic_vector (W-1 downto 0);
-				koutput		: out std_logic_vector (W-1 downto 0);
-				
-				vdinput		: in std_logic_vector (W-1 downto 0);	-- V.D input.
-				vdoutput	: out std_logic_vector (W-1 downto 0)	-- Selected dot product.
+			clk,rst		: in std_logic;
+			scanOut		: in std_logic; -- This signals overrides the 'signed greater or equal than' internal function and allows vdinput to flow upwards.
+			nxtSphere	: in std_logic; -- Controls when the sphere goes to the next Row. 
+			pipeOn		: in std_logic; -- Enables / Disable the upwarding flow.
+			kinput		: in std_logic_vector (W1-1 downto 0);
+			koutputhor	: out std_logic_vector (W1-1 downto 0);
+			koutputver	: out std_logic_vector (W1-1 downto 0);	-- K input  flowing to the next floor upstairs (but waits one clock). 
+			vdinput		: in std_logic_vector (W1-1 downto 0);	-- V.D input.
+			vdoutput	: out std_logic_vector (W1-1 downto 0)	-- Selected dot product.
 				
 				
 	);
-	end port;
+	
 end entity;
 
 
 architecture rtl of kComparisonCell is 
 
 	signal ssge32	: std_logic;	-- Signed "Greater or equal  than" signal.
-
+	signal sena		: std_logic;	-- Enable internal signal
+	signal disc		: std_logic;
 begin
 
-	comparison : sge32 port map (
+	-- Enable resolution
+	sena <= pipeOn or scanOut;
+	disc <= ssge32 or scanOut;
+	
+	-- Enable
+	kcomp : sge32 
+	port map ( 
 		dataa	=> vdinput,
 		datab 	=> kinput,
 		AgeB	=> ssge32
-		);
-
+	);
+	
+	
+	
 	-- When ssge32 (greater or equal signal) is set then V.D > kte, therefore intersection is confirmed and  V.D is to be shifted to the distance comparison grid.
-	selector : process (rst,clk,ssg32,pipeOn)
+	selector : process (rst,clk,sena)
 	begin
 			
 		if rst='0' then
 				
 			-- At the beginning set the Maximum over Maximum distance.
-			vdoutput <= '0' & (others =>'1');
+			vdoutput (W1-1)<= '0';
+			vdoutput (W1-2 downto 0) <= (others => '1');
+			koutputver <= (others => '0');
 				
-		elsif rising_edge(clk) and pipeOn ='1' then 
+		elsif rising_edge(clk) and sena='1' then 
+			
+			-- Flowing Upwards of vinput.
+			koutputver <= kinput;
 				
-			if ssge32 = '1' then -- If VD ids grater or equal than K .....
+			if  disc='1' then -- If VD ids grater or equal than K .....
 				vdoutput <= vdinput;
 			else
-				vdoutput <= '0' & (others =>'1');
+				vdoutput(W1-1) <= '0';
+				vdoutput(W1-2 downto 0)<=( others => '1' );
 			end if;
 			
 		end if;
 		
 	end process;
-			
+
+
+
+	
 	-- Behavioral : When nxtSphere is set, the Sphere and its K constant should go the the next row
+	kHorizontalPipeStage : if RK = "yes" generate
 
-	kPipeStage : process (clk,rst,nxtSphere)
-	begin
-	
-		if rst='0' then
-			
-			koutput <= (others => '0');
+		process (clk,rst,nxtSphere)
+		begin
 		
-		elsif rising_edge(clk) and nxtSphere ='1' then
+			if rst='0' then
 				
-			koutput <= kinput;
+				koutputhor <= (others => '0');
 			
-		end if;
+			elsif rising_edge(clk) and nxtSphere ='1' then
+					
+				koutputhor <= kinput;
+				
+			end if;
+		
+		end process;
 	
-	end process;
-
+	end generate kHorizontalPipeStage;
 
 
 end rtl;
